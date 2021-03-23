199a91de0c813a7e6e73e0fd09eeceb859c82cea
ramya <ramya.rolly@gmail.com>
2021-03-23 13:54:56 -0400

    paypal checkout by react

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const  firebaseConfig = {
+    apiKey: "AIzaSyBUhJ3bF0PBn6zKyIrZwNhongTZ_h2jbQ4",
+    authDomain: "checkout-d35a4.firebaseapp.com",
+    projectId: "checkout-d35a4",
+    storageBucket: "checkout-d35a4.appspot.com",
+    messagingSenderId: "99709782741",
+    appId: "1:99709782741:web:7f053b9c796797f23cf05c",
+    measurementId: "G-L7GYPCF93Z"
+};
